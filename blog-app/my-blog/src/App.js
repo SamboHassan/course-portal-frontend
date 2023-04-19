26dbe48f2e1bbf5dad2a53bb0c712b3c93a0f15b
Hassan Sambo <sambohasan@gmail.com>
2023-04-19 18:09:14 +0100

    add initial my-blog app files

---
+++ blog-app/my-blog/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Awesome blog</h1>
+      <div id="page-body">
+        Welcome to my blog!
+      </div>
+    </div>
+  );
+}
+
+export default App;
