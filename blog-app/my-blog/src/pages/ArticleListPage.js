84fe567840d36c526d16c39f4891503699023e98
Hassan Sambo <sambohasan@gmail.com>
2023-05-10 18:23:11 +0100

    update article list page

--- blog-app/my-blog/src/pages/ArticleListPage.js
+++ blog-app/my-blog/src/pages/ArticleListPage.js
@@ -1,17 +1,19 @@
 import articles from "./article-content";
-import {Link} from "react-router-dom"
+import ArticlesList from "../components/ArticlesList";
 
 const ArticleListPage = () => {
 
     return (
         <>
             <h1>Articles</h1>
-            {articles.map(article => (
-                <Link to={`/articles/${article.name}`}>
+            <ArticlesList articles={articles} />
+            
+            {/* {articles.map(article => (
+                <Link key={article.name } to={`/articles/${article.name}`}>
                     <h3>{article.title}</h3>
                     <p>{article.content[0].substring(0, 150)}...</p>
                 </Link>   
-            ))}
+            ))} */}
         </>
     );
 
