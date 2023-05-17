b03cbfd0c0829ff61b070ac2bf712b35250e9e47
Hassan Sambo <sambohasan@gmail.com>
2023-05-17 14:18:31 +0100

    add new comment endpoint

--- blog-app/my-blog-backend/src/server.js
+++ blog-app/my-blog-backend/src/server.js
@@ -1,36 +1,21 @@
-import express from "express";
+import express from 'express';
 
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
-const app = express()
-app.use(express.json())
-
-
-// app.get("/hello", (req, res) => {
-//     res.send("Hello!");
-// });
-
-app.post("/hello", (req, res) => {
-    // console.log(req.body)
-    res.send(`Hello! ${req.body.name}`);
-});
-
-app.get("/hello", (req, res) => { 
-    // const name = req.params.name;
-    const { name } = req.params
-    res.send(`Hello ${name}!!`);
-});
-
-//BEGIN
+const app = express();
+app.use(express.json());
 
 app.put('/api/articles/:name/upvote', (req, res) => {
     const { name } = req.params;
@@ -38,12 +23,42 @@ app.put('/api/articles/:name/upvote', (req, res) => {
 
     if (article) {
         article.upvotes += 1;
-        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
     } else {
         res.send('That article doesn\'t exist');
     }
 });
 
-app.listen(8000, () => {
-    console.log("Server is listening on port 8000")
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist!');
+    }
 });
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+});
+
+
+// app.get("/hello", (req, res) => {
+//     res.send("Hello!");
+// });
+
+// app.post("/hello", (req, res) => {
+//     // console.log(req.body)
+//     res.send(`Hello! ${req.body.name}`);
+// });
+
+// app.get("/hello", (req, res) => { 
+//     // const name = req.params.name;
+//     const { name } = req.params
+//     res.send(`Hello ${name}!!`);
+// });
