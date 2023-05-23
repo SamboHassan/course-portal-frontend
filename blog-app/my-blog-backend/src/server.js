c79be016273873bb58896caa21ec8761bd127e82
Hassan Sambo <sambohasan@gmail.com>
2023-05-23 17:55:03 +0100

    refactor: add firebase admin config to server.js file

--- blog-app/my-blog-backend/src/server.js
+++ blog-app/my-blog-backend/src/server.js
@@ -1,10 +1,10 @@
 import fs from 'fs';
-import admin, { auth } from 'firebase-admin';
+import admin from 'firebase-admin';
 import express from 'express';
 import { db, connectToDb } from './db.js';
 
 const credentials = JSON.parse(
-    fs.readFileSync('../credentials.json')
+    fs.readFileSync('./credentials.json')
 );
 admin.initializeApp({
     credential: admin.credential.cert(credentials),
@@ -20,9 +20,11 @@ app.use(async (req, res, next) => {
         try {
             req.user = await admin.auth().verifyIdToken(authtoken);
         } catch (e) {
-            res.sendStatus(400);
+            return res.sendStatus(400);
         }
     }
+
+    req.user = req.user || {};
 
     next();
 });
@@ -35,7 +37,7 @@ app.get('/api/articles/:name', async (req, res) => {
 
     if (article) {
         const upvoteIds = article.upvoteIds || [];
-        article.canUpvote = uid && !upvoteIds.include(uid);
+        article.canUpvote = uid && !upvoteIds.includes(uid);
         res.json(article);
     } else {
         res.sendStatus(404);
@@ -58,7 +60,7 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
     if (article) {
         const upvoteIds = article.upvoteIds || [];
-        const canUpvote = uid && !upvoteIds.include(uid);
+        const canUpvote = uid && !upvoteIds.includes(uid);
    
         if (canUpvote) {
             await db.collection('articles').updateOne({ name }, {
@@ -97,3 +99,168 @@ connectToDb(() => {
         console.log('Server is listening on port 8000');
     });
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import fs from 'fs';
+// import path from 'path';
+// import admin from 'firebase-admin';
+// import express from 'express';
+// import 'dotenv/config';
+// import { db, connectToDb } from './db.js';
+
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const credentials = JSON.parse(
+//     fs.readFileSync('./credentials.json')
+// );
+// admin.initializeApp({
+//     credential: admin.credential.cert(credentials),
+// });
+
+// const app = express();
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, '../build')));
+
+// app.get(/^(?!\/api).+/, (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build/index.html'));
+// })
+
+// app.use(async (req, res, next) => {
+//     const { authtoken } = req.headers;
+
+//     if (authtoken) {
+//         try {
+//             req.user = await admin.auth().verifyIdToken(authtoken);
+//         } catch (e) {
+//             return res.sendStatus(400);
+//         }
+//     }
+
+//     req.user = req.user || {};
+
+//     next();
+// });
+
+// app.get('/api/articles/:name', async (req, res) => {
+//     const { name } = req.params;
+//     const { uid } = req.user;
+
+//     const article = await db.collection('articles').findOne({ name });
+
+//     if (article) {
+//         const upvoteIds = article.upvoteIds || [];
+//         article.canUpvote = uid && !upvoteIds.includes(uid);
+//         res.json(article);
+//     } else {
+//         res.sendStatus(404);
+//     }
+// });
+
+// app.use((req, res, next) => {
+//     if (req.user) {
+//         next();
+//     } else {
+//         res.sendStatus(401);
+//     }
+// });
+
+// app.put('/api/articles/:name/upvote', async (req, res) => {
+//     const { name } = req.params;
+//     const { uid } = req.user;
+
+//     const article = await db.collection('articles').findOne({ name });
+
+//     if (article) {
+//         const upvoteIds = article.upvoteIds || [];
+//         const canUpvote = uid && !upvoteIds.includes(uid);
+   
+//         if (canUpvote) {
+//             await db.collection('articles').updateOne({ name }, {
+//                 $inc: { upvotes: 1 },
+//                 $push: { upvoteIds: uid },
+//             });
+//         }
+
+//         const updatedArticle = await db.collection('articles').findOne({ name });
+//         res.json(updatedArticle);
+//     } else {
+//         res.send('That article doesn\'t exist');
+//     }
+// });
+
+// app.post('/api/articles/:name/comments', async (req, res) => {
+//     const { name } = req.params;
+//     const { text } = req.body;
+//     const { email } = req.user;
+
+//     await db.collection('articles').updateOne({ name }, {
+//         $push: { comments: { postedBy: email, text } },
+//     });
+//     const article = await db.collection('articles').findOne({ name });
+
+//     if (article) {
+//         res.json(article);
+//     } else {
+//         res.send('That article doesn\'t exist!');
+//     }
+// });
+
+// const PORT = process.env.PORT || 8000;
+
+// connectToDb(() => {
+//     console.log('Successfully connected to database!');
+//     app.listen(PORT, () => {
+//         console.log('Server is listening on port ' + PORT);
+//     });
+// })
